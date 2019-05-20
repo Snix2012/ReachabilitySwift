716c83bb153e5e78261bcff033d0b9e275a3fc39
Claire Roughan <claire@fsbtech.com>
2019-05-20 16:30:34 +0100

    Initial Commit

---
+++ SwiftReachability/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftReachability
+//
+//  Created by Claire Roughan on 20/05/2019.
+//  Copyright © 2019 Claire Roughan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
